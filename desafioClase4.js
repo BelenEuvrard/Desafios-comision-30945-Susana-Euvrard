dfe36ee8f7f5f0e953643e0d94df7297c162b6cc
BelenEuvrard <beleneuvrard@gmail.com>
2022-04-06 15:04:48 -0300

    reentrega Manejo de Archivos en Javascript

--- desafioClase4.js
+++ desafioClase4.js
@@ -1,128 +1,138 @@
-
-
-
+const fs = require('fs');
 
 class Contenedor {
-
-    constructor(archivo){
-        this.archivo = archivo
-        
+    constructor (archivo) {
+        this.archivo = archivo;
     }
-   
+
     save(producto) {
-        let productos = [];
-        productos.push(producto)
-        producto.id = this.generarId(productos) + 1;
-        const fs = require('fs')
+        let productos = []; 
+               
+        productos = fs.readFile(this.archivo, 'utf-8', (error, contenido)  => {
+            if (error) {
+                console.log('No existen Productos.')
+                producto.id = 1;
+                productos = [];
+                productos.push(producto);
+                this.guardarProductos(productos);
+            } else {
+                try {
+                    productos = JSON.parse(contenido);
+                    producto.id = this.generarId(productos) + 1;
+                    productos.push(producto);
+                    this.guardarProductos(productos); 
+                } catch {
+                    fs.unlink(this.archivo, error => {
+                        if (error === false) {
+                            console.log('Archivo Eliminado.');
+                        }
+                    })
+                }
+                           
+            }
+        });
+    }
+    generarId(productos) {
+        let id = 1;
+        productos.map(prod => {
+            if (prod.id>id) {
+                id = prod.id;
+            }
+        })
+        return id;
+    }
+    guardarProductos(productos) {
+              
         fs.writeFile(this.archivo, JSON.stringify(productos), error =>{
             if (error) {
                 console.log('Error al Guardar el Archivo.');
             } else {
-                
                 console.log('Productos Guardados.');
             }
         })
-    
     }
-        
-     generarId(productos) {
-            let id = 1;
-            productos.map(prod => {
-                if (prod.id>id) {
-                    id = prod.id;
-                }
-            })
-            return id;
-        }
-    
     getById(Number, producto) {
-            let productos = []; 
-            const fs = require('fs');       
-            productos = fs.readFile(this.archivo, 'utf-8', (error, contenido)  => {
-                if (error) {
-                    console.log('No existen Productos.')
-                    producto(null);
-                } else {
-                    productos = JSON.parse(contenido);
-                    const prod = productos.find( prod => prod.id==Number);
-                    producto(prod);
-                }
-            });
-        }   
-
+        let productos = []; 
+               
+        productos = fs.readFile(this.archivo, 'utf-8', (error, contenido)  => {
+            if (error) {
+                console.log('No existen Productos.')
+                producto(null);
+            } else {
+                productos = JSON.parse(contenido);
+                const prod = productos.find( prod => prod.id==Number);
+                producto(prod);
+            }
+        });
+    }
     getAll(all) {
-            let productos = []; 
-            const fs = require('fs');       
-            productos = fs.readFile(this.archivo, 'utf-8', (error, contenido)  => {
-                if (error) {
-                    console.log('No existen Productos.')
-                    all(null);
-                } else {
-                    productos = JSON.parse(contenido);
-                    all(productos);
-                }
-            });
-        } 
-        deleteById(Number) {
-            let productos = []; 
-            const fs = require('fs');       
-            productos = fs.readFile(this.archivo, 'utf-8', (error, contenido)  => {
-                if (error) {
-                    console.log('No existen Productos. Nada para Borrar')
-                } else {
-                    productos = JSON.parse(contenido);
-                    const prod = productos.find( prod => prod.id==Number);
-                    try {
-                        if (prod.length==0) {
-                            console.log(`No se Encontró el Producto con ID ${Number}`);
-                        } else {
-                            const i = productos.indexOf(prod);
-                            console.log(`Indice ${i}`)
-                            productos.splice(i, 1);
-                            this.guardarProductos(productos)
-                            console.log(`Producto con ID ${Number} Eliminado ¡¡¡¡`);
-                        }
-                    } catch {
+        let productos = []; 
+            
+        productos = fs.readFile(this.archivo, 'utf-8', (error, contenido)  => {
+            if (error) {
+                console.log('No existen Productos.')
+                all(null);
+            } else {
+                productos = JSON.parse(contenido);
+                all(productos);
+            }
+        });
+    }
+
+    deleteById(Number) {
+        let productos = []; 
+              
+        productos = fs.readFile(this.archivo, 'utf-8', (error, contenido)  => {
+            if (error) {
+                console.log('No existen Productos. Nada para Borrar')
+            } else {
+                productos = JSON.parse(contenido);
+                const prod = productos.find( prod => prod.id==Number);
+                try {
+                    if (prod.length==0) {
                         console.log(`No se Encontró el Producto con ID ${Number}`);
-                    }                
-                }
-            });
-        }
-    
-        deleteAll() {
-            const fs = require('fs');       
-            fs.unlink(this.archivo, error => {
-                if (error) {
-                    console.log('No se Pudieron Eliminar los Productos.');
-                } else {
-                    console.log('Productos Eliminados.');
-                }
-            })  
-        }
-   
+                    } else {
+                        const i = productos.indexOf(prod);
+                        console.log(`Indice ${i}`)
+                        productos.splice(i, 1);
+                        this.guardarProductos(productos)
+                        console.log(`Producto con ID ${Number} Eliminado ¡¡¡¡`);
+                    }
+                } catch {
+                    console.log(`No se Encontró el Producto con ID ${Number}`);
+                }                
+            }
+        });
+    }
+
+    deleteAll() {
+               
+        fs.unlink(this.archivo, error => {
+            if (error) {
+                console.log('No se Pudieron Eliminar los Productos.');
+            } else {
+                console.log('Productos Eliminados.');
+            }
+        })  
+    }
 }
 
-
-const nuevoProducto = {
+const nuevoProd = {
     title: 'SAMSUNG GALAXY A12 - SM -A127M AZUL',                                                                                                                                 
     price: 32900,                                                                                                                                     
-    thumbnail: 'imagen',   
+    thumbnail: 'imagen',             
 }
+const phone = new Contenedor('productos.json')
+phone.save(nuevoProd);
 
-const telefonos = new Contenedor('productos.json')
+/* phone.getById(2, (prod) => {
+    console.log('Producto Econtrado: '+JSON.stringify(prod));
+})
 
-telefonos.save(nuevoProducto)
+phone.getAll((prods) => {
+    console.log('Conjunto de Productos: '+JSON.stringify(prods));
+})
 
-/* se dejo comentado las llamadas a modo que quede el productos.json con la primer llamada save*/
-
-//telefonos.getById(2, (prod) => {
-//    console.log('Producto Econtrado: '+JSON.stringify(prod));
-//})
-
-//telefonos.getAll((prods) => {
-//    console.log('Conjunto de Productos: '+JSON.stringify(prods));
-//})
-
-//telefonos.deleteById(2);
-//telefonos.deleteAll();
-
+phone.deleteById(2);
+phone.deleteAll();
+ */
